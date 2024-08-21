7ceec81c18c8add12155ad5694ec71c9f9ecd4af
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-08-20 23:02:54 -0300

    ADD new project
    Adicionando novo projeto "Estado Lâmpada" em pequenos projetos de POO.

---
+++ Projetos-Pequenos-OOP/src/com/evelyn/projects/lampadaligadaoudesligada/Lampada.java
@@ -0,0 +1,17 @@
+package com.evelyn.projects.lampadaligadaoudesligada;
+
+public class Lampada {
+
+    boolean ligarLampada(String ligado){
+
+        if(ligado.equalsIgnoreCase("ligar")){
+
+            return true;
+        
+        } else {
+            return false;
+        }
+        
+    }
+
+}
