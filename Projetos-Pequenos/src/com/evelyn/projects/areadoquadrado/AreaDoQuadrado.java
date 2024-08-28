79778fac6d5115c474bce6f001493ad89f925a63
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-08-27 23:08:26 -0300

    Update código-fonte "AreaDoCirculo"
    Melhorando a descrição e adicionando funcionalidade para permitir que o usuário digite novamente caso tenha errado.

--- Projetos-Pequenos/src/com/evelyn/projects/areadoquadrado/AreaDoQuadrado.java
+++ Projetos-Pequenos/src/com/evelyn/projects/areadoquadrado/AreaDoQuadrado.java
@@ -2,7 +2,7 @@
  * Author: Evelyn
  * Date: 16/07/2024
  * Language: Java
- * Description: É um programa que armazena a altura e a base de um quadrado, e calcula sua área e o dobro da área
+ * Description: Programa que armazena a altura e a base de um quadrado, e calcula sua área e o dobro da área
  */
 
 package com.evelyn.projects.areadoquadrado;
@@ -13,31 +13,41 @@ public class AreaDoQuadrado {
     
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+      System.out.println("\n============== Área de um quadrado ==============\n");
+
+      Scanner input = new Scanner(System.in);
+      
+      boolean isValido = true; //Flag(bandeira)
+      double base = 0, altura = 0;
         
-          System.out.println("\n============== Área de um quadrado ==============\n");
+      do{
 
-          System.out.print(">> Digite a base: ");
-          double base = input.nextDouble();
+        System.out.print(">> Digite a base: ");
+        base = input.nextDouble();
 
-          System.out.print(">> Digite a altura: ");
-          double altura = input.nextDouble();
+        System.out.print(">> Digite a altura: ");
+        altura = input.nextDouble();
 
-          System.out.println("=================================================\n");
 
-          if(base != altura){
+        if(base != altura){
 
-            System.out.println("Error: < Para calcular a área do quadrado, a base e a altura precisa, tem a mesma medida. >");
+          System.out.println("--> Erro: Para calcular a área do quadrado, a base e a altura precisa, tem a mesma medida.\n");
 
-          } else {
+          isValido = false;
 
-            double area = base * altura;      
-            System.out.println(">> A área do quadrado é: " + area);
+        } else {
+          isValido = true;
+        }
+        
+      } while(!isValido);
+      
+      
+      System.out.println("=================================================\n");
+      
+      double area = base * altura;      
+      System.out.println(">> A área do quadrado é: " + area);
 
-            System.out.println(">> E seu dobro é: " + area * 2 + "\n");
-          }
-          
-          
+      System.out.println(">> E seu dobro é: " + area * 2 + "\n");
       
     }
 }
