1072a862af2058262f1ee3826cf9d6569e0b4169
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-09-13 23:53:54 -0300

    files: add QtdParImpar.java

---
+++ Projetos-Pequenos/src/com/evelyn/projects/imparpar/QtdParImpar.java
@@ -0,0 +1,44 @@
+package com.evelyn.projects.imparpar;
+
+import java.util.Scanner;
+
+/**
+ * <h1> Par ou ímpar </h1>
+ * <p>Programa que recebe dez números, e imprime a quantidade de pares e ímpares</p>
+ * 
+ * <h4>Language: Java</h4>
+ * 
+ * @author Evelyn Mendes
+ * @version 1.0
+ * @since 27/08/2024
+ */
+
+public class QtdParImpar {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("=============== Quantidade de números Pares e ímpares ===============");
+        Scanner input = new Scanner(System.in);
+
+        int[] numeros = new int[10];
+        int countPares = 0, countImpares = 0;
+        
+        for(int i = 0; i < 10; i++){
+
+            System.out.print(">> Digite o " + (i+1) + "º número: ");
+            numeros[i] = input.nextInt();
+
+            if(numeros[i] % 2 == 0){
+              countPares++;
+            
+            } else {
+
+              countImpares++;
+            }
+        }
+
+        System.out.println("\n=====================================================================");
+        System.out.println(">> Quantidade de pares: " + countPares);
+        System.out.println(">> Quantidade de ímpares: " + countImpares);
+    }
+}
