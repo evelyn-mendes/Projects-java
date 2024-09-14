66d039927561415208daa4f7b04a97207d0569fe
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-09-13 23:35:30 -0300

    files: update FahParaCel.java

--- Projetos-Pequenos/src/com/evelyn/projects/fahrenheitparacelsius/FahParaCel.java
+++ Projetos-Pequenos/src/com/evelyn/projects/fahrenheitparacelsius/FahParaCel.java
@@ -1,14 +1,17 @@
-/*
- * Author: Evelyn
- * Date: 16/07/2024
- * Language: Java
- * Description: É um programa que armazena a temperatura em fahrenheit, e a converte para celsius
- */
-
 package com.evelyn.projects.fahrenheitparacelsius;
 
 import java.util.Scanner;
 
+/**
+ * <h1> Fahrenheit para Celsius </h1>
+ * <p>É um programa que armazena a temperatura em Fahrenheit e a converte para Celsius</p>
+ * 
+ * <h4>Language: Java</h4>
+ * 
+ * @author Evelyn Mendes
+ * @version 1.0
+ * @since 16/07/2024
+ */
 public class FahParaCel {
     
         public static void main(String[] args) {
@@ -17,9 +20,9 @@ public class FahParaCel {
             Scanner input = new Scanner(System.in);
 
             System.out.print(">> Digite a temperatura em Fahrenheit: ");
-            double farenheit = input.nextDouble();
+            double fahrenheit = input.nextDouble();
 
-            double celsius = (5 * (farenheit - 32)/9);
+            double celsius = (5 * (fahrenheit - 32)/9);
 
             System.out.println("======================================================\n");
             System.out.printf(">> A temperatura em Celsius é: %.3fºC\n\n", celsius);       
