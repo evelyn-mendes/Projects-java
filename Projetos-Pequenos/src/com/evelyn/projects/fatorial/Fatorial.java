9f98a758c2236c7456960e84991bda408c63cfd0
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-09-21 20:40:37 -0300

    files: create Fatorial.java
    Subindo novo projeto em projetos-pequenos, no pacote "fatorial".

---
+++ Projetos-Pequenos/src/com/evelyn/projects/fatorial/Fatorial.java
@@ -0,0 +1,42 @@
+package com.evelyn.projects.fatorial;
+
+import java.util.Scanner;
+
+/**
+ * <h1> Fatorial </h1>
+ * 
+ * <p>É um programa que armazena um número e imprime o seu fatorial.</p>
+ * 
+ * <h4>Language: Java </h4>
+ * 
+ * @author Evelyn Mendes
+ * @version 1.1
+ * @since 21/09/2024
+ */
+
+public class Fatorial {
+    
+    public static void main(String[] args) {
+
+        System.out.println("================== Fatorial ==================");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(">> Fatorial de: ");
+        int num = input.nextInt();
+
+        int fatorial = 1;
+
+        System.out.print(num + "! = ");
+
+        for(int i = num; i > 1; i--){
+
+            System.out.print(i + " x ");
+
+            fatorial *= i;
+        }
+
+        System.out.println("1 = " + fatorial + "\n");
+
+        input.close();
+    }
+}
