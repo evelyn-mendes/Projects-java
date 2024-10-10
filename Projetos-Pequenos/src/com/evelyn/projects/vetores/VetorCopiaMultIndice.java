66e433d9de9de5bfbce0098cc60e21dcc19020dd
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-10-09 22:18:05 -0300

    files: create VetorCopiaMultIndice.java
    Subindo um novo projeto em pequenos projetos java, do package vetores.

---
+++ Projetos-Pequenos/src/com/evelyn/projects/vetores/VetorCopiaMultIndice.java
@@ -0,0 +1,45 @@
+package com.evelyn.projects.vetores;
+
+import java.util.Scanner;
+
+/**
+ * <p> Um programa tem um vetor B com o mesmo tipo, tamanho do vetor A, mas com os elementos sendo a multiplicação dos valores de A pelo índice. </p>
+ * 
+ * <h4>Language: Java </h4>
+ * 
+ * @author Evelyn Mendes
+ * @version 1.0
+ * @since 03/10/2024
+ */
+
+public class VetorCopiaMultIndice {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+
+        for(int i = 0; i < vetorA.length; i++) {
+
+            System.out.print("Digite o " + (i+1) + " elemento: ");
+            vetorA[i] = input.nextInt();
+
+            vetorB[i] = vetorA[i] * i;
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < vetorA.length; i++ ){ 
+
+            System.out.println("Vetor A: " + vetorA[i]);
+            System.out.println("Vetor B: " +  vetorB[i] + "\n");
+
+        }
+
+        System.out.println();
+
+        input.close();
+    }
+}
