d3a72ec73b4f6e8e12b7f75012883bd95832764a
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-10-09 22:24:33 -0300

    files: create VetorCopiaSoma.java
    Subindo um novo projeto em pequenos projetos java, do package vetores.

---
+++ Projetos-Pequenos/src/com/evelyn/projects/vetores/VetorCopiaSoma.java
@@ -0,0 +1,51 @@
+package com.evelyn.projects.vetores;
+
+import java.util.Scanner;
+
+/**
+ * <p> Um programa que tem os vetores B e C, com o mesmo tipo e tamanho do vetor A. 
+ * O vetor B recebe os mesmos valores do vetor A, e o vetor C armazena a soma dos valores de A e B. </p>
+ * 
+ * <h4>Language: Java </h4>
+ * 
+ * @author Evelyn Mendes
+ * @version 1.0
+ * @since 03/10/2024
+ */
+
+public class VetorCopiaSoma {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+        int[] vetorC = new int[vetorA.length];
+
+        for(int i = 0; i < vetorA.length; i++ ){
+
+            System.out.print("\n>> Digite o " + (i+1) + " elemento do vetor A: ");
+            vetorA[i] = input.nextInt();
+
+            System.out.print(">> Digite o " + (i+1) + " elemento do vetor B: ");
+            vetorB[i] = input.nextInt();
+
+            System.out.println();
+
+            vetorC[i] = vetorA[i] + vetorB[i];
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < vetorA.length; i++ ){ 
+
+            System.out.println("Vetor A: " + vetorA[i]);
+            System.out.println("Vetor B: " +  vetorB[i]);
+            System.out.println("Soma, vetor C: " + vetorC[i] + "\n");
+
+        }
+
+        input.close();
+    }
+}
