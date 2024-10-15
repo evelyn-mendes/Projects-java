5b6ada79fe595036b464c019979630f3da0cea81
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-10-15 20:36:54 -0300

    files: create SomaElementos.java
    Subindo um novo projeto em pequenos projetos java, do package vetores.

---
+++ Projetos-Pequenos/src/com/evelyn/projects/vetores/SomaElementos.java
@@ -0,0 +1,36 @@
+package com.evelyn.projects.vetores;
+
+import java.util.Scanner;
+
+/**
+ *  <p> Um programa que percorre um vetor Numbers, soma os elemtos desse vetor e imprime a soma deles. </p>
+ * 
+ * <h4> Language: Java </h4>
+ * 
+ * @author Evelyn Mendes 
+ * @version 1.0
+ * @since 28/09/2024
+ */
+
+public class SomaElementos {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+        int sum = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+
+            System.out.print("\n>> Digite o " + (i+1) + " elemento do vetor A: ");
+            numbers[i] = input.nextInt();
+
+            sum += numbers[i];
+        }
+
+        System.out.println("\n>>A soma de todos os números é: " + sum + "\n");
+
+        input.close();
+    }
+}
