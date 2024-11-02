bcce39d2c5724dab6af28d6d2bdaca0912b738e8
evelyn-mendes <evelyn.lavinia1712@gmail.com>
2024-11-01 21:52:45 -0300

    files: create Porcent0e1.java
    Subindo um novo projeto em pequenos projetos java, do package vetores. Que calcula a porcentagem de ocorrências entre 0s e 1s.

---
+++ Projetos-Pequenos/src/com/evelyn/projects/vetores/Porcent0e1.java
@@ -0,0 +1,44 @@
+package com.evelyn.projects.vetores;
+
+/**
+ * <p> Esta aplicação gera números aleatórios de 0 e 1, armazena-os em um vetor e, ao final, calcula e exibe a porcentagem de ocorrências de cada número no vetor. </p>
+ * 
+ * <h4> Language: Java </h4>
+ * 
+ * @author Evelyn Mendes
+ * @version 1.0
+ * @since 29/09/2024
+ */
+
+public class Porcent0e1 {
+     
+    public static void main(String[] args) {
+        
+        int[] numbers = new int[10];
+        int count0 = 0, count1 = 0;
+
+        for(int i = 0; i < numbers.length; i++ ) {
+
+            numbers[i] = numberGenerator();
+        
+            if(numbers[i] == 0) {
+                count0 ++;
+            } else {
+                count1 ++;
+            }
+        }
+
+        System.out.print("\n>> Vetor A: ");
+        for(int numero : numbers) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.println("\n>> Porcentagem de números 0: " + (count0*100/10) + "%");
+        System.out.println(">> Porcentagem de números 1: " + (count1*100/10) + "%\n");
+    }
+
+    public static int numberGenerator () {
+
+        return (int)Math.round(Math.random()); // O casting é necessário, pois o retorno será do tipo "long".
+    }
+}
